d0e6200081c050c6d719d10794928261d2002e9e
haln14 <haln14@fpt.com.vn>
2022-11-18 16:13:19 +0700

    pylint  and black  setup with pre-commit

--- name/test_pylint.py
+++ name/test_pylint.py
@@ -1,7 +1,8 @@
-'''module for test pylint'''
+"""module for test pylint"""
+
 
 def my_function():
-    """ An example for a Pylint demonstration."""
+    """An example for a Pylint demonstration."""
     my_sum1 = 3 + 4
-    print (f"The sum is ${my_sum1}.")
+    print(f"The sum is ${my_sum1}.")
     return my_sum1
